b92318cb1e1157f743879c218b6c0571365761d6
mpizzzle <m@michaelpercival.xyz>
2022-08-04 00:05:47 +0100

    initial commit for cron expression parser (project setup)

---
+++ src/test/java/cron/parser/AppTest.java
@@ -0,0 +1,20 @@
+package cron.parser;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        Assertions.assertTrue( true );
+    }
+}
